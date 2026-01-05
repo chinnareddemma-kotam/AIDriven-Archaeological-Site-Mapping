dec115cb806942587aab8222bd8e6770161d3a9c
Chinna reddemma kotam <chinnareddemma57@gmail.com>
2026-01-05 16:50:01 +0530

    Update streamlit_app.py

--- Soil_detection/streamlit_app.py
+++ Soil_detection/streamlit_app.py
@@ -3,7 +3,10 @@ import torch
 from PIL import Image
 import torchvision.transforms as transforms
 from model import build_model
+import os
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+MODEL_PATH = os.path.join(BASE_DIR, "models", "best.pth")
 # ----------------------------
 # Device
 # ----------------------------
